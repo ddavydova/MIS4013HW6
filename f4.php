95720cd8aec80bc3be8b7e6a6ea357beb5908233
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:54:42 -0600

    Update f4.php

--- f4.php
+++ f4.php
@@ -1,4 +1,33 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
 
-<?php include("footer.php");?>
+<head>
+	<!-- Load plotly.js into the DOM -->
+	<script src='https://cdn.plot.ly/plotly-2.16.1.min.js'></script>
+</head>
+
+<body>
+	<div id='myDiv'><!-- Plotly chart will be drawn inside this DIV --></div>
+</body>
+<script>
+  var trace1 = {
+  x: [1, 2, 3, 4],
+  y: [10, 11, 12, 13],
+  mode: 'markers',
+  marker: {
+    size: [40, 60, 80, 100]
+  }
+};
+
+var data = [trace1];
+
+var layout = {
+  title: 'Marker Size',
+  showlegend: false,
+  height: 600,
+  width: 600
+};
+
+Plotly.newPlot('myDiv', data, layout);
+</script>
+</html>
