f5cdb7a92605e0bacff9226506aa94b3a722ef67
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:07:28 -0600

    Update f1.php

--- f1.php
+++ f1.php
@@ -24,5 +24,18 @@ var granimInstance = new Granim({
 });
   </script>
 
+<canvas id="canvas-complex"></canvas>
+<sctyle>
+#canvas-basic {
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+}
+</sctyle>
      
 <?php include("footer.php");?>
