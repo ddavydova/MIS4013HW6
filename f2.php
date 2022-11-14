634aca3126717e84fa820764f86bd264c13c2322
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:42:14 -0600

    Update f2.php

--- f2.php
+++ f2.php
@@ -1,5 +1,55 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
    
+{% import 'components/Plate/Plate.html' as Plate %}
 
-<?php include("footer.php");?>
+{% macro render(id) %}
+  <section id="{{ id }}" class="{{ id }}">
+    <article class="PlateWrapper">
+      {{ Plate.render('Three', {level: 'Bottom', draggable: true}) }}
+      {{ Plate.render('Two',   {level: 'Middle', draggable: true}) }}
+      {{ Plate.render('One',   {heading: 'hello', level: 'Top', draggable: true}) }}
+    </article>
+  </section>
+{% endmacro %}
+
+
+<script>
+   // eslint-disable-next-line import/no-unresolved
+import {Draggable} from '@shopify/draggable';
+
+// eslint-disable-next-line shopify/strict-component-boundaries
+import Plate from '../../components/Plate';
+
+export default function Home() {
+  const containerSelector = '#Home .PlateWrapper';
+  const container = document.querySelector(containerSelector);
+
+  if (!container) {
+    return false;
+  }
+
+  const draggable = new Draggable(container, {
+    draggable: '.Plate',
+  });
+  const plates = new Plate(container);
+
+  // --- Draggable events --- //
+  draggable.on('drag:start', (evt) => {
+    plates.setThreshold();
+    plates.setInitialMousePosition(evt.sensorEvent);
+  });
+
+  draggable.on('drag:move', (evt) => {
+    // rAF seems to cause the animation to get stuck?
+    // requestAnimationFrame(() => {});
+    plates.dragWarp(evt.source, evt.sensorEvent);
+  });
+
+  draggable.on('drag:stop', () => {
+    plates.resetWarp();
+  });
+
+  return draggable;
+}
+</script>
