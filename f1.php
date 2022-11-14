034eedc50dcf812b3c6a0b183f48e5cf6a3404ad
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:11:56 -0600

    Update f1.php

--- f1.php
+++ f1.php
@@ -1,5 +1,7 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
 
-     
+<script src="https://rawgit.com/WeiChiaChang/Easter-egg/master/easter-eggs-collection.js"></script>
+import collection from 'easter-egg-collection'
+
 <?php include("footer.php");?>
