f33cc6370e503a7d6bb24159f2544ccad212624c
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:53:41 -0600

    Update links.php

--- links.php
+++ links.php
@@ -12,8 +12,5 @@
     <li class="nav-item">
 	    <a class="nav-link <?php if ($CURRENT_PAGE == "Form") {?>active<?php }?>" href="f4.php">Function 4</a>
 	  </li>
-    <li class="nav-item">
-	    <a class="nav-link <?php if ($CURRENT_PAGE == "Form") {?>active<?php }?>" href="f5.php">Function 5</a>
-	  </li>
 	</ul>
 </div>
