fe99492498160255dceb9a72c576ccb03637f12c
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 12:57:23 -0600

    Update f1.php

--- f1.php
+++ f1.php
@@ -1,20 +1,60 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
 
-<script>
-function randomValues() {
-  anime({
-    targets: '.random-demo .el',
-    translateX: function() {
-      return anime.random(0, 270);
-    },
-    easing: 'easeInOutQuad',
-    duration: 750,
-    complete: randomValues
-  });
-}
+const config = {
+  type: 'bar',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Floating Bar Chart'
+      }
+    }
+  }
+};
 
-randomValues();
-</script>
+const DATA_COUNT = 7;
+const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+const labels = Utils.months({count: 7});
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => {
+        return [Utils.rand(-100, 100), Utils.rand(-100, 100)];
+      }),
+      backgroundColor: Utils.CHART_COLORS.red,
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => {
+        return [Utils.rand(-100, 100), Utils.rand(-100, 100)];
+      }),
+      backgroundColor: Utils.CHART_COLORS.blue,
+    },
+  ]
+}; 
+
+const actions = [
+  {
+    name: 'Randomize',
+    handler(chart) {
+      chart.data.datasets.forEach(dataset => {
+        dataset.data = chart.data.labels.map(() => {
+          return [Utils.rand(-100, 100), Utils.rand(-100, 100)];
+        });
+      });
+      chart.update();
+    }
+  },
+];
+
 
 <?php include("footer.php");?>
