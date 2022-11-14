83fe73db0d4bdc6a56bd10839230ad146dcbac9b
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:08:55 -0600

    Update f1.php

--- f1.php
+++ f1.php
@@ -1,41 +1,5 @@
 <?php include("header.php");?>
 <?php include("links.php");?>
 
-<SCRIPT>
-var granimInstance = new Granim({
-    element: '#canvas-complex',
-    direction: 'left-right',
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                [
-                    { color: '#833ab4', pos: .2 },
-                    { color: '#fd1d1d', pos: .8 },
-                    { color: '#38ef7d', pos: 1 }
-                ], [
-                    { color: '#40e0d0', pos: 0 },
-                    { color: '#ff8c00', pos: .2 },
-                    { color: '#ff0080', pos: .75 }
-                ],
-            ]
-        }
-    }
-});
-  </script>
-
-<canvas id="canvas-complex"></canvas>
-<style>
-#canvas-basic {
-    position: absolute;
-    display: block;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-}
-</style>
      
 <?php include("footer.php");?>
