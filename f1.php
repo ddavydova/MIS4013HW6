0a63a42c9dfb659fe33cc70faf74d15b49c0b140
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:39:28 -0600

    Update f1.php

--- f1.php
+++ f1.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="utf-8" />
 
-
+<main>
 <body style="center">
     <script src="sketch.js"></script>
  <script>
@@ -40,7 +40,8 @@ function findRhymes() {
   }
 }
   </script>
-
+    </main>
+<div>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
@@ -59,6 +60,6 @@ $d2=ceil(($d1-time())/60/60/24);
 echo "There are " . $d2 ." days until Christmas!";
 ?>
   
-</div>
+    </div></div>
 </body>
 </html>
