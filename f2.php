6dc9eeda3c4a84b6f2867cce5cbe3387d5e97831
Dasha Davydova <dasha.davydova@ou.edu>
2022-11-14 13:47:37 -0600

    Update f2.php

--- f2.php
+++ f2.php
@@ -2,3 +2,11 @@
 <?php include("links.php");?>
    
 
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+<img class="nature" src="img_snowtops.jpg" width="100%">
+<img class="nature" src="img_mountains.jpg" width="100%">
+<img class="nature" src="img_nature.jpg" width="100%">
+
+<script>
+w3.slideshow(".nature", 1500);
+</script>
